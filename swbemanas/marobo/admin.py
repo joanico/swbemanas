3798cd428243a3baeab7b9ad6b5ab0a47a7774b4
Joanico-Barros <joanicobarros@gmail.com>
2017-05-15 22:39:04 +0900

    adding field for sucos and aldeia

--- swbemanas/marobo/admin.py
+++ swbemanas/marobo/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Population
+from .models import Population, Suco, Aldeia
 
 class PopulationAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Population, PopulationAdmin)
+
+
+class SucoAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Suco, SucoAdmin)
+
+
+class AldeiaAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Aldeia, AldeiaAdmin)
