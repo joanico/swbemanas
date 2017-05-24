1bed6dea249a2ae98b6efec89011d39b89b86bce
Joanico-Barros <joanicobarros@gmail.com>
2017-05-24 22:54:25 +0900

    modified static files in base templates

--- swbemanas/swbemanas/settings.py
+++ swbemanas/swbemanas/settings.py
@@ -121,11 +121,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media"),
+
+LOGIN_REDIRECT_URL = '/'
 
 LEAFLET_CONFIG = {
     'SPATIAL_EXTENT': (124.3, -9.9, 127.4, -8.0),
