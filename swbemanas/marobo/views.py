ee7e770e00bc3c6dd1f07c06263c5ab5c8632ae4
Joanico-Barros <joanico@catalpa.io>
2018-05-11 09:28:59 +0900

    refactoring code in django view

--- swbemanas/marobo/views.py
+++ swbemanas/marobo/views.py
@@ -1,10 +1,10 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-#from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
 #from django.views.generic.list import ListView
 #from django.core.urlresolvers import reverse_lazy
-#from marobo.models import Population
+from .models import Suco, Population, Aldeia
 
 
 @login_required(login_url="login/")
@@ -14,3 +14,14 @@ def home(request):
 
 class Mapa(TemplateView):
     template_name = 'marobo/mapamarobo.html'
+
+
+class PopulationView(TemplateView):
+    template_name = 'marobo/suco.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PopulationView, self).get_context_data(**kwargs)
+        context['sucos'] = Suco.objects.all().order_by('name')
+        context['populations'] = Population.objects.all()
+        context['aldeias'] = Aldeia.objects.all()
+        return context
