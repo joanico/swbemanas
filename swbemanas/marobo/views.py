9bf6a11c07ea6c5cddce0367a68374a2b1364ba5
joanico <joanico@catalpa.io>
2021-11-15 10:26:04 +0900

    add login page

--- swbemanas/marobo/views.py
+++ swbemanas/marobo/views.py
@@ -1,9 +1,9 @@
 from django.views.generic.base import TemplateView
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView
-#from django.views.generic.list import ListView
-#from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 from .models import Post, PostImage
 
 
@@ -21,3 +21,21 @@ def detail_view(request, id):
         'post':post,
         'photos':photos
     })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('blog')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        messages.info(request, "You have successfully logged out.")
+    return redirect('logout')
